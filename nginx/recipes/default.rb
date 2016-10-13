fd6a5d4f88c1beb26a14d7fefc2b7b29bf612260
deven1986 <rock.deven1986@gmail.com>
2016-10-13 18:34:14 -0500

    second commit

--- nginx/recipes/default.rb
+++ nginx/recipes/default.rb
@@ -1,8 +1,12 @@
-#
-# Cookbook Name:: nginx
-# Recipe:: default
-#
-# Copyright 2016, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
+package "epel-release" do 
+	action :install
+end
+
+package "nginx" do
+	action :install
+end
+
+service "nginx" do
+	action [ :enable, :start ]
+end
+
